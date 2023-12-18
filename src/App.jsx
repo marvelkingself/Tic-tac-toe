ac63fd27342b0576863081815ffb481bdc8dd38a
Abdul Baroon <99256762+marvelkingself@users.noreply.github.com>
2023-12-18 18:18:17 +0530

    fit push of tic-tac-toe

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Board from './Components/Board'
+
+const App = () => {
+  return (
+    <div className='m-5'>
+      <Board />
+    </div>
+  )
+}
+
+export default App
